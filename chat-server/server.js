a5320fb5ff8beb24a305c9fd4f6f9ef9e62a4b62
gootieno <geoff.otieno7@gmail.com>
2023-08-15 22:32:18 -0700

    sockets working in backend

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,25 +1,35 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5002" }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: "http://localhost:3000" }));
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 const comments = [];
-
-app.post("/comments", function (req, res) {
-  const comment = req.body;
-  console.log("comment express ", comment);
-  comments.push(comment);
-
-  // talk to ai server
-  res.json(comment);
-});
 
 app.get("/comments", function (req, res) {
   res.json({ messages: comments });
 });
 
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", (message) => {
+    console.log("message ", message);
+    comments.push(message);
+    socket.broadcast.emit("message", message);
+  });
+});
+
 const port = 5000;
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+server.listen(port, () => console.log(`listening on port ${port}`));
